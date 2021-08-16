708af9a2940b1a314ba6b4ac029e5dd82281bf60
Thomas Jollans <tjol@tjol.eu>
2021-08-17 00:49:19 +0200

    Wrap output

--- www/wasm_io.js
+++ www/wasm_io.js
@@ -2,7 +2,8 @@ export function write_rfunge_output(s)
 {
     let output_elem = document.getElementById("output")
 
-    output_elem.innerHTML = output_elem.innerHTML + s
+    output_elem.innerHTML = (output_elem.innerHTML
+        + s.replaceAll("<", "&lt;").replaceAll(">", "&gt;"))
 }
 
 export function write_rfunge_warning(msg)
