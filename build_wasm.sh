010701d9aa02b09ed3c6016aabe4672be5c91630
Thomas Jollans <tjol@tjol.eu>
2021-08-16 23:47:09 +0200

    Add licensing info and source download to web page

--- build_wasm.sh
+++ build_wasm.sh
@@ -2,4 +2,6 @@
 
 cd "`dirname $0`"
 
-exec wasm-pack build --target=web --out-dir=www/wasm_pkg
+wasm-pack build --target=web --out-dir=www/wasm_pkg && \
+git archive --format=tar --prefix=rfunge/ HEAD | xz - > www/rfunge.src.tar.xz && \
+git rev-parse --short=8 HEAD > www/wasm_pkg/rev.hash.txt
